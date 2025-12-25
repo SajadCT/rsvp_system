64a87e73ca8eb031a2ee9acaa154c20eb45c3f9d
Sajad CT <sajad.ct098@gmail.com>
2025-12-25 22:35:38 +0530

    initialize gin server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"log"
 	"rsvp/backend/internal/database"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	database.ConnectDB()
-	log.Println("Database connected successfully")
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "ok",
+		})
+	})
+	log.Println("Server running on :8080")
+	r.Run(":8080")
 }
